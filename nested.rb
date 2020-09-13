ea724566bc7cdf6344bdaeffadb140b1b2985646
Tabatha Roman <romantabatha17@gmail.com>
2020-09-13 16:01:15 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,7 @@ def adding_matz
 # add the following information to the top level of programmer_hash
 # :yukihiro_matsumoto => {
 #   :known_for => "Ruby",
-#    :languages => ["LISP", "C"]
+#     :languages => ["LISP", "C"]
 # }
 
 	programmer_hash = 
@@ -82,7 +82,11 @@ def adding_matz
         }
      }
 
-    
+programmer_hash[:yukihiro_matsumoto] => {
+  :known_for => "Ruby",
+    :languages => ["LISP", "C"]
+}
+
 end
 
 def changing_alan
